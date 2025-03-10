d2d2247676dd44dff703d8f734e76a4a476d0ad7
Luisander2651 <luisander2651@gmail.com>
2025-03-09 21:37:56 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
